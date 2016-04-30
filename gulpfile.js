9af36a1b0b46ae66e49466fa37f638f942077e26
ricecreamdude <ho.joshua4@gmail.com>
2016-04-29 21:45:54 -0700

    Researching Yeoman gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('clean:tmp', function (cb) {
 });
 
 gulp.task('start:client', ['start:server', 'styles'], function () {
-  openURL('http://localhost:9000');
+  openURL('http://localhost:8080');
 });
 
 gulp.task('start:server', function() {
@@ -78,7 +78,7 @@ gulp.task('start:server', function() {
     root: [yeoman.app, '.tmp'],
     livereload: true,
     // Change this to '0.0.0.0' to access the server from outside.
-    port: 9000
+    port: 8080
   });
 });
 

66f3dc542054f7d9c539a81abef43477295f6942
ricecreamdude <ho.joshua4@gmail.com>
2016-04-29 19:25:25 -0700

    Angular Yo Setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ec2portfolioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ec2portfolioApp
+ */
+angular.module('ec2portfolioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ec2portfolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ec2portfolioApp
+ */
+angular.module('ec2portfolioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
